9938140746955233d00355bd0e04cbb89ec54233
Matthew Mo <matthew.mo520@gmail.com>
2025-01-25 16:22:59 -0500

    Updated Phone background

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const PhoneForm = () => {
     return (
-        <div className="flex items-center justify-center w-[314px] h-[675px] border-3 border-black rounded-[50px]">
-            <div className="flex justify-center w-[292px] h-[655px] bg-[#FF9C67] opacity-50 rounded-[50px]">
+        <div className="bg-cover bg-center flex items-center justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px] ">
                 <form className="flex flex-col items-center space-y-4">
                     <div className="w-[240px] h-[39px] mb-2.5">
                         <label htmlFor="companyName" className="mb-2.5">Company Name:</label>
@@ -15,7 +14,6 @@ const PhoneForm = () => {
                     </div>
                     <button type="submit" className="w-[197px] h-[39px] rounded-[100px] bg-white">Search</button>
                 </form>
-            </div>
         </div>
     );
 };
