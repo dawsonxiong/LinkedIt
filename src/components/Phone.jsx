771d13d5efd969dacc4c630acb0ab8dc26998daa
Matthew Mo <matthew.mo520@gmail.com>
2025-01-25 16:32:08 -0500

    Update Phone.jsx

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const PhoneForm = () => {
     return (
-        <div className="bg-cover bg-center flex items-center justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px] ">
-                <form className="flex flex-col items-center space-y-4">
-                    <div className="w-[240px] h-[39px] mb-2.5">
+        <div class="bg-cover bg-center flex items-center justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px] ">
+                <form class="flex flex-col items-center space-y-4">
+                    <div class="w-[240px] h-[39px] mb-2.5">
                         <label htmlFor="companyName" className="mb-2.5">Company Name:</label>
-                        <input type="text" id="companyName" name="companyName" className="bg-white w-full rounded" />
+                        <input type="text" id="companyName" name="companyName" class="bg-white w-full rounded-[100px]" />
                     </div>
-                    <div className="w-[240px] h-[39px]">
+                    <div class="w-[240px] h-[39px]">
                         <label htmlFor="companyPosition">Company Position:</label>
-                        <input type="text" id="companyPosition" name="companyPosition" className="bg-white w-full rounded" />
+                        <input type="text" id="companyPosition" name="companyPosition" class="bg-white w-full rounded-[100px]" />
                     </div>
-                    <button type="submit" className="w-[197px] h-[39px] rounded-[100px] bg-white">Search</button>
+                    <button type="submit" class="w-[197px] h-[39px] rounded-[100px] bg-white">Search</button>
                 </form>
         </div>
     );
