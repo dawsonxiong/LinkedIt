c3f43cc882fd18d4f3451a05c44e01402e2bb43a
Matthew Mo <matthew.mo520@gmail.com>
2025-01-25 18:38:32 -0500

    transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
 import Info from './components/info.jsx'
 import PhoneForm from './components/phone.jsx';
 import Background from './assets/bg.png'
 import Email from './components/EmailList.jsx';
 import Profile from './components/Profile.jsx';
 
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file
 
 function App() {
+  const [showInfo, setShowInfo] = useState(true);
+  const [spacing, setSpacing] = useState('-space-x-120');
+  const [spaceX, setSpaceX] = useState('space-x-60');
+  const [rMargin, setrMargin] = useState('pr-38');
+  const [animate, setAnimate] = useState(false);
+
+  const handleSearchClick = () => {
+    setShowInfo(false);
+    setAnimate(true);
+    setTimeout(() => {
+      setSpacing('-space-x-160');
+      setSpaceX('space-x-100');
+      setrMargin('pr-75');
+      setAnimate(false);
+    }, 300); 
+  };
+
   return (
-    <div className="bg-[url(./assets/bg.png)] flex items-center justify-center w-screen h-screen space-x-60 pr-38">
-    {/* <Profile/> */}
-    <Info/>
-      <div className ="flex -space-x-120">
-        <Email/>
-        <PhoneForm/>
+    <div className={`bg-[url('./assets/bg.png')] flex items-center justify-center w-screen h-screen ${spaceX} ${rMargin}`}>
+      {showInfo ? <Info /> : <Profile />}
+      <div className={`flex ${spacing}`}>
+        <Email />
+        <PhoneForm onSearch={handleSearchClick} className={animate ? 'slide' : ''} />
       </div>
-      
     </div>
   );
 }
--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,22 +1,39 @@
+// PhoneForm.jsx
 import React from 'react';
 
-const PhoneForm = () => {
-    return (
-        <div class="bg-cover bg-center flex  justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px] ">
-                <form class="flex flex-col items-center mt-35 space-y-5">
-                    <label>Company Name:</label>
-                    <div class="">
-                        <input type="text" id="companyName" name="companyName" class="bg-white w-[240px] h-[39px] rounded-[100px] font-serif" />
-                    </div>
-                    
-                        <label>Company Position:</label>
-                    <div class="">
-                        <input type="text" id="companyPosition" name="companyPosition" class="bg-white w-[240px] h-[39px] rounded-[100px] font-serif" />
-                    </div>
-                    <button type="submit" class="w-[197px] h-[39px] rounded-[100px] bg-white">Search</button>
-                </form>
+const PhoneForm = ({ onSearch }) => {
+  const handleSearch = (e) => {
+    e.preventDefault();
+    onSearch();
+  };
+
+  return (
+    <div className="bg-cover bg-center flex justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px]">
+      <form className="flex flex-col items-center mt-35 space-y-5" onSubmit={handleSearch}>
+        <label>Company Name:</label>
+        <div>
+          <input
+            type="text"
+            id="companyName"
+            name="companyName"
+            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif"
+          />
         </div>
-    );
+        <label>Company Position:</label>
+        <div>
+          <input
+            type="text"
+            id="companyPosition"
+            name="companyPosition"
+            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif"
+          />
+        </div>
+        <button type="submit" className="w-[197px] h-[39px] rounded-[100px] bg-white">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default PhoneForm;
