a466b20a6a02414389746eb7620c31dd6062bb93
Matthew Mo <matthew.mo520@gmail.com>
2025-01-25 20:09:45 -0500

    updated button

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -10,25 +10,25 @@ const PhoneForm = ({ onSearch }) => {
   return (
     <div className="bg-cover bg-center flex justify-center bg-[url(./assets/Landingpage.png)] w-[316px] h-[675px]">
       <form className="flex flex-col items-center mt-35 space-y-5" onSubmit={handleSearch}>
-        <label>Company Name:</label>
+        <label className="font-serif">Company Name:</label>
         <div>
           <input
             type="text"
             id="companyName"
             name="companyName"
-            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif"
+            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif pl-2"
           />
         </div>
-        <label>Company Position:</label>
+        <label className="font-serif">Company Position:</label>
         <div>
           <input
             type="text"
             id="companyPosition"
             name="companyPosition"
-            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif"
+            className="bg-white w-[240px] h-[39px] rounded-[100px] font-serif pl-2"
           />
         </div>
-        <button type="submit" className="w-[197px] h-[39px] rounded-[100px] bg-white">
+        <button type="submit" className="font-serif w-[197px] h-[39px] rounded-[100px] bg-white hover:bg-gray-200 transition duration-300">
           Search
         </button>
       </form>
