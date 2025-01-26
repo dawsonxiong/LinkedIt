fb9b4df52d6dd916f14d5cbd4e955edbb4a7b0ef
Matthew Mo <matthew.mo520@gmail.com>
2025-01-26 00:21:18 -0500

    Update EmailList.jsx

--- src/components/EmailList.jsx
+++ src/components/EmailList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Email = ({ onHomeClick }) => {
     return (
-        <div className="bg-cover bg-center flex items-top justify-center bg-[url(./assets/Infopage.png)] w-[316px] h-[675px]">
+        <div className="bg-cover bg-center flex items-top justify-center bg-[url(./assets/Infopage.png)] w-[316px] h-[675px] overflow-auto">
             <div className="w-10 h-4"></div>
             <div className="top container">
                 <button className="btn mt-5" onClick={onHomeClick}>
